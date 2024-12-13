912c4624233224a407a536295811db3866afbc78
Arya Gharachorloo <arya.gharachorloo@menloschool.org>
2024-12-12 21:09:19 -0800

    Added comments

--- src/BST.java
+++ src/BST.java
@@ -48,18 +48,23 @@ public class BST {
      */
     public boolean search(int val) {
         // TODO: Complete the search function
+        // Call the helper function
         return search(root, val);
     }
     private boolean search (BSTNode root, int val) {
+        // Check to see if the current node is null, if it is, return false
         if (root == null) {
             return false;
         }
+        // If the value of the node is equal to the desired value, return true
         if (root.getVal() == val) {
             return true;
         }
+        // If the value is less than the current node, then recurse to the left node
         else if (val < root.getVal()) {
             return search(root.getLeft(), val);
         }
+        // Otherwise, recurse to the right node.
         return search(root.getRight(), val);
     }
 
@@ -68,14 +73,20 @@ public class BST {
      */
     public ArrayList<BSTNode> getInorder() {
         // TODO: Complete inorder traversal
+        // Make a list and call the helper function
         ArrayList<BSTNode> list = new ArrayList<>();
         getInorder(root, list);
+        // Return the list of values in Inorder.
         return list;
     }
     private void getInorder(BSTNode root, ArrayList<BSTNode> list) {
+        // While the node is not null
         if (root != null) {
+            // Recurse as left as possible
             getInorder(root.getLeft(), list);
+            // Add the node
             list.add(root);
+            // recurse as right as possible
             getInorder(root.getRight(), list);
         }
     }
@@ -86,14 +97,20 @@ public class BST {
      */
     public ArrayList<BSTNode> getPreorder() {
         // TODO: Complete preorder traversal
+        // Make a new list and call helper function
         ArrayList<BSTNode> list = new ArrayList<>();
         getPreorder(root, list);
+        // return the list completed
         return list;
     }
     private void getPreorder(BSTNode root, ArrayList<BSTNode> list) {
+        // If the root is not null
         if (root != null) {
+            // Add the node
             list.add(root);
+            // recurse to the left and add each node
             getPreorder(root.getLeft(), list);
+            // Recurse to the right and add each node
             getPreorder(root.getRight(), list);
         }
     }
@@ -104,14 +121,20 @@ public class BST {
      */
     public ArrayList<BSTNode> getPostorder() {
         // TODO: Complete postorder traversal
+        // Make a new list and call helper function
         ArrayList<BSTNode> list = new ArrayList<>();
         getPostorder(root, list);
+        // return the list completed
         return list;
     }
     private void getPostorder(BSTNode root, ArrayList<BSTNode> list) {
+        // if the root is not null
         if (root != null) {
+            // Recurse to the left
             getPostorder(root.getLeft(), list);
+            // Recurse to the right
             getPostorder(root.getRight(), list);
+            // Add the node to the list after going both ways
             list.add(root);
         }
     }
@@ -124,17 +147,25 @@ public class BST {
      */
     public void insert(int val) {
         // TODO: Complete insert
+        // Call helper function
         root = insert(root, val);
     }
     private BSTNode insert(BSTNode root, int val) {
+        // If the node is null
         if (root == null) {
+            // Add the new node with the desired value
             return new BSTNode(val);
         }
+        // If the desired value is less than that of the node
         if (val < root.getVal()) {
+            // Recurse to the left
             root.setLeft(insert(root.getLeft(), val));
+        // if the desired value is greater than that of the node
         } else if (val > root.getVal()) {
+            // recurse to the right
             root.setRight(insert(root.getRight(), val));
         }
+        // return the root
         return root;
     }
 
